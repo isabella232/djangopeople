b37df7f51654e349a58b7bf0c2116308b5f69889
Tim Graham <timograham@gmail.com>
2016-10-27 18:40:05 -0400

    Skip envdir on Heroku

--- manage.py
+++ manage.py
@@ -6,11 +6,12 @@ import envdir
 
 
 if __name__ == "__main__":
-    if 'test' in sys.argv:
-        env_dir = os.path.join('tests', 'env')
-    else:
-        env_dir = 'env'
-    envdir.read(env_dir)
+    if 'HEROKU_POSTGRESQL_VIOLET_URL' not in os.environ:
+        if 'test' in sys.argv:
+            env_dir = os.path.join('tests', 'env')
+        else:
+            env_dir = 'env'
+        envdir.read(env_dir)
 
     from django.core.management import execute_from_command_line
 
